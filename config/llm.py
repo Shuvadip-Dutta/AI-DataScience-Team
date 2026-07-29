da130af7ef2d433805a7e1ae920bb4370b73c2bd
Shuvadip Dutta <105166892+Shuvadip-Dutta@users.noreply.github.com>
2026-07-29 20:00:37 +0530

    add config

---
+++ config/llm.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+load_dotenv()
+
+os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+llm=ChatGroq(model_name="llama-3.3-70b-versatile")
